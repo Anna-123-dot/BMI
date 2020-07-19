fb38f48fcc7abad9f7286fefdc200c18c22ef0a0
Anna <aprusik@tlen.pl>
2020-07-19 12:48:44 +0200

    zadanie 14

--- src/zad13Main.java
+++ src/zad13Main.java
@@ -1,7 +1,11 @@
 public class zad13Main {
     public static void main(String[] args) {
-        zad13 zad13 = new zad13();
-        zad13.run();
+       // zad13 zad13 = new zad13();
+       // zad13.run();
 
+        zad14 zad14 = new zad14();
+        zad14.Chars aChar = zad14.getChar();
+
+        System.out.println(zad14.getResult(aChar));
     }
 }
