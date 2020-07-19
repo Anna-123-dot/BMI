5ccb2b5c143f4d935b64be94ccae38f3d2e8f004
Anna <aprusik@tlen.pl>
2020-07-19 12:18:18 +0200

    zadanie 13

--- src/zad13.java
+++ src/zad13.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class zad13 {
-    public static void main(String[] args) {
+
+    //Napisz program, który „się jąka”, to znaczy pobiera użytkownika tekst (zmienną typu String), a następnie wypisuje podany tekst, w którym każde słowo wypisane jest po dwa razy. Przykładowo, dla wejścia: „To jest mój test” program powinien wypisać „To To jest jest mój mój test test”.
+    public String pobierz() {
+
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
 
     }
+
+   private String[] split(String text) {
+        return text.split(" ");
+
+    }
+
+    private void wypisz(String[] text) {
+        StringBuilder sb = new StringBuilder();
+
+        for (String word : text) {
+            sb.append(word)
+                    .append(" ")
+                    .append(word)
+                    .append(" ");
+        }
+        System.out.println(sb.toString());
+    }
+    public void run () {
+        wypisz(split(pobierz()));
+    }
 }
+
+
---
+++ src/zad13Main.java
@@ -0,0 +1,7 @@
+public class zad13Main {
+    public static void main(String[] args) {
+        zad13 zad13 = new zad13();
+        zad13.run();
+
+    }
+}
